36525cc810ce616c7fdb026f0c2dcaa45770d556
Daniel Greuel <dgreuel@gmail.com>
2019-11-09 23:11:56 -0600

    update dependencies, version, use yarn pnp

--- README.md
+++ README.md
@@ -4,16 +4,16 @@
 
 ## Building
 
-1.  Clone repo
-2.  `yarn install`
-3.  `yarn dev` to compile once or `yarn watch` to run the dev task in watch mode
-4.  `yarn build` to build a production (minified) version
+1. Clone repo
+2. `yarn install`
+3. `yarn dev` to compile once or `yarn watch` to run the dev task in watch mode
+4. `yarn build` to build a production (minified) version
 
 ## Installation
 
-1.  Complete the steps to build the project above
-2.  Go to [_chrome://extensions_](chrome://extensions) in Google Chrome
-3.  With the developer mode checkbox ticked, click **Load unpacked extension...** and select the _dist_ folder from this repo
+1. Complete the steps to build the project above
+2. Go to [_chrome://extensions_](chrome://extensions) in Google Chrome
+3. With the developer mode checkbox ticked, click **Load unpacked extension...** and select the _dist_ folder from this repo
 
 ## Usage
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,46 @@
-const path = require('path');
+const path = require("path");
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 module.exports = {
-  entry: {
-    popup: path.join(__dirname, 'src/popup/index.tsx'),
-    background: path.join(__dirname, 'src/background.ts'),
-    contentScript: path.join(__dirname, 'src/contentScript.ts')
-  },
-  output: {
-    path: path.join(__dirname, 'dist/js'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules)|(dist)/,
-        test: /\.t|jsx?$/,
-        use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }]
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader' // Creates style nodes from JS strings
-          },
-          {
-            loader: 'css-loader' // Translates CSS into CommonJS
-          },
-          {
-            loader: 'sass-loader' // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  }
+   entry: {
+      popup: path.join(__dirname, "src/popup/index.tsx"),
+      background: path.join(__dirname, "src/background.ts"),
+      contentScript: path.join(__dirname, "src/contentScript.ts")
+   },
+   output: {
+      path: path.join(__dirname, "dist/js"),
+      filename: "[name].js"
+   },
+   module: {
+      rules: [
+         {
+            exclude: /(node_modules)|(dist)/,
+            test: /\.t|jsx?$/,
+            loader: require.resolve("ts-loader"),
+            options: PnpWebpackPlugin.tsLoaderOptions({})
+         },
+         {
+            exclude: /node_modules/,
+            test: /\.scss$/,
+            use: [
+               {
+                  loader: "style-loader" // Creates style nodes from JS strings
+               },
+               {
+                  loader: "css-loader" // Translates CSS into CommonJS
+               },
+               {
+                  loader: "sass-loader" // Compiles Sass to CSS
+               }
+            ]
+         }
+      ]
+   },
+   resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      plugins: [PnpWebpackPlugin]
+   },
+   resolveLoader: {
+      plugins: [PnpWebpackPlugin.moduleLoader(module)]
+   }
 };
